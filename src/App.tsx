f66724d0919bcc14c3fcf0d57be6bfb7d1eefea2
kalz2q <katsu95i@gmail.com>
2022-11-30 03:43:48 +0900

    first2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = document.getElementById("text").value;
+
+  const readaloud = () => {
+    const uttr = new SpeechSynthesisUtterance();
+    uttr.text = text;
+    // 発言を再生
+    window.speechSynthesis.speak(uttr);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <input type="text" id="text" name="text" value="吾輩は猫である。" placeholder="読み上げたいテキストを入力してください" />
+      {/* <button onclick="readAloud()">読み上げる</button> */}
+      <p>上記テキストを読み上げます。「読み上げる」ボタンをクリックしてください。</p>
+    </div >
   );
 }
 
